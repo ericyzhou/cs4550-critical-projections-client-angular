f06278da8fe3224e6852bfa989f7a0f1f5931473
jackferrari <jackxferrari@gmail.com>
2020-12-09 15:15:29 -0500

    Update IMDBSearchService.ts

--- src/services/IMDBSearchService.ts
+++ src/services/IMDBSearchService.ts
@@ -3,10 +3,10 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class IMDBSearchService {
   findMovieByTitle = (movieTitle: any) =>
-    fetch(`http://www.omdbapi.com/?apikey=b41175d7&type=movie&s=${movieTitle}`)
+    fetch(`https://www.omdbapi.com/?apikey=b41175d7&type=movie&s=${movieTitle}`)
       .then(response => response.json())
 
   findMovieById = (movieId: any) =>
-    fetch(`http://www.omdbapi.com/?apikey=b41175d7&plot=full&i=${movieId}`)
+    fetch(`https://www.omdbapi.com/?apikey=b41175d7&plot=full&i=${movieId}`)
       .then(response => response.json())
 }
