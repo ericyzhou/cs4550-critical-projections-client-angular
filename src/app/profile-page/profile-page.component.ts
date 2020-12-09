ced73c4d2625d94dc21be32ab8a6d31609c4fad3
jackferrari <jackxferrari@gmail.com>
2020-12-08 19:02:36 -0500

    minor fixes

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  user = {username: '', password: '', email: '', role: ''};
+  user = {username: 'temp', password: '1234', email: '1324@5678.com', role: 'ADMIN'};
 
   constructor() { }
 
