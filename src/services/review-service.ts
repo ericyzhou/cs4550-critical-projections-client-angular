a34ca14a3d8476733939d1374a03135de7594372
ZachBlauvelt <blauvelt.z@northeastern.edu>
2020-12-11 21:26:34 -0500

    Updated review and comment services to use Heroku Spring app

--- src/services/review-service.ts
+++ src/services/review-service.ts
@@ -1,25 +1,27 @@
 import {Injectable} from '@angular/core';
 
+const url = 'https://critical-projections-server.herokuapp.com/api';
+
 @Injectable()
 export class ReviewService {
   fetchReviewsForMovie = (mid: any, count: number) =>
-    fetch(`http://localhost:8080/api/movies/${mid}/reviews/${count}`)
+    fetch(`${url}/movies/${mid}/reviews/${count}`)
       .then(response => response.json())
 
   fetchCriticReviewsForMovie = (mid: any, count: number) =>
-    fetch(`http://localhost:8080/api/movies/${mid}/reviews/critic/${count}`)
+    fetch(`${url}/movies/${mid}/reviews/critic/${count}`)
       .then(response => response.json())
 
   fetchUserReviewsForMovie = (mid: any, count: number) =>
-    fetch(`http://localhost:8080/api/movies/${mid}/reviews/user/${count}`)
+    fetch(`${url}/movies/${mid}/reviews/user/${count}`)
       .then(response => response.json())
 
   fetchReviewsForUser = (uid: any) =>
-    fetch(`http://localhost:8080/api/users/${uid}/reviews`)
+    fetch(`${url}/users/${uid}/reviews`)
       .then(response => response.json())
 
   createReview = (title: string, body: string, rating: number, movieId: string) =>
-    fetch(`http://localhost:8080/api/reviews`, {
+    fetch(`${url}/reviews`, {
       method: 'POST',
       body: JSON.stringify({movieId, rating, Title: title, body}),
       headers: {
@@ -29,7 +31,7 @@ export class ReviewService {
       .then(response => response.json())
 
   updateReview = (review: any) =>
-    fetch(`http://localhost:8080/api/reviews/${review.id}`, {
+    fetch(`${url}/reviews/${review.id}`, {
       method: 'PUT',
       body: JSON.stringify(review),
       headers: {
@@ -39,7 +41,7 @@ export class ReviewService {
       .then(response => response.json())
 
   deleteReview = (review: any) =>
-    fetch(`http://localhost:8080/api/reviews/${review.id}`, {
+    fetch(`${url}/reviews/${review.id}`, {
       method: 'DELETE'
     })
       .then(response => response.json())
