3b3edafecf469f90fba4b4ff6fbdbaad429e525c
jackferrari <jackxferrari@gmail.com>
2020-12-13 05:36:08 -0500

    Tabs

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+   settings = false;
+   reviews = true;
+   comments = true;
+   currTab = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  switchToSettings = () => {
+    this.settings = false;
+    this.reviews = true;
+    this.comments = true;
+  }
+
+  switchToReviews = () => {
+    this.settings = true;
+    this.reviews = false;
+    this.comments = true;
+  }
+
+  switchToComments = () => {
+    this.settings = true;
+    this.reviews = true;
+    this.comments = false;
+  }
+}
+
+enum ProfileTabs {
+  Settings,
+  Review,
+  Comments
 }
