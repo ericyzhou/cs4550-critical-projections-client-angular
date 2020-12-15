9c44c5ce26bb28c44197ce09bc8d2549c0ee4f3e
Eric Zhou <ericyzhou00@gmail.com>
2020-12-14 19:41:43 -0500

    profile redirect

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -39,6 +39,8 @@ export class ProfilePageComponent implements OnInit {
           default:
             this.switchToError();
         }
+      } else {
+        this.switchToSettings();
       }
     });
   }
