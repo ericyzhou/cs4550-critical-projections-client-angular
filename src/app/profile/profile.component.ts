f990d63976f88c0a0a3913853b4dcc1f12d54fdb
Eric Zhou <ericyzhou00@gmail.com>
2020-12-14 19:24:43 -0500

    profile redirect

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user-service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser()
+      .then(response => {
+        if (response.response === 1) {
+          this.router.navigate([`/profile/${response.user.id}`]);
+        } else {
+          this.router.navigate([`/signup`]);
+        }});
+  }
+
+}
